16270e7a25178cb5aa8c0f992e9cca40075655a8
pjy <2454499877@qq.com>
2022-07-01 10:33:17 +0800

    v1

---
+++ gulimall-ware/src/main/java/com/pjy/WareApplication.java
@@ -0,0 +1,11 @@
+package com.pjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WareApplication.class, args);
+    }
+}
